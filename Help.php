21c8675e6f308e93549603bac95f33843572803b
rajesh <developerrajeshgupta@gmail.com>
2025-11-28 11:48:00 +0530

    add content in help file

--- Help.php
+++ Help.php
@@ -1,3 +1,4 @@
 <?php
 echo "<h1>Custom Frame Work made by Software Developer: Rajesh Kumar Gutpta</h1>";
 echo "add collaborations to test as different developer";
+echo "add development Branch to work all developer on this branch not main branch";
